3659857fa637fdf9be0883f047a8892cbe67b94c
manjoos <71747405+0xmanjoos@users.noreply.github.com>
2021-03-07 13:34:30 -0800

    Create 01.asm

---
+++ win32/01.asm
@@ -0,0 +1,30 @@
+bits 32
+;
+; nasm -f win32 01.asm
+; golink 01.obj kernel32.dll user32.dll /entry:_start
+;
+[SECTION .drctve info align=8]
+db "kernel32.dll", 0x20
+db "user32.dll", 0x20   ; space terminated strings
+
+
+extern ExitProcess
+extern MessageBoxA
+
+section .data
+    caption: db "you done got hacked now!!!", 0
+    message: db "Hello World!", 0
+
+
+section .text
+global _start
+
+_start:
+    push DWORD 0
+    push DWORD caption
+    push DWORD message
+    push DWORD 0
+    call MessageBoxA
+
+    push DWORD 0
+    call ExitProcess
