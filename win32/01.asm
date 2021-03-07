2fac4b44af6822ba9ccbab65446b4b233596bd54
manjoos <71747405+0xmanjoos@users.noreply.github.com>
2021-03-07 13:35:32 -0800

    Update 01.asm

--- win32/01.asm
+++ win32/01.asm
@@ -20,7 +20,7 @@ section .text
 global _start
 
 ; i dont like how everything in windowws x86 is a dword
-; cant they just use bytes liek the rest of us
+; me no liek
 _start:
     push DWORD 0
     push DWORD caption
